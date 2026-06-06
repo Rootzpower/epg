0e6d581ed43e7c78d90960ee6d8a5647e1f090a6
Rootz <15384112+Rootzpower@users.noreply.github.com>
2026-06-06 11:55:06 +0100

    Update generate-all-logos-mosaics.php

--- utilities/generate-all-logos-mosaics.php
+++ utilities/generate-all-logos-mosaics.php
@@ -3,13 +3,10 @@
 Based on the original script from the tv-logos project.
 @see https://github.com/tv-logo/tv-logos
 */ 
-
 error_reporting(E_ALL);
-
 if (PHP_SAPI !== 'cli') {
     die("This script must be ran from the command line.");
 }
-
 $settings = array(
     'countriesFolders' => array(
         __DIR__ . '/../logos',
@@ -17,93 +14,85 @@ $settings = array(
     'outputFilename' => '0_all_logos_mosaic.md',
     'cols' => 6,
 );
-
 function listAllFiles(string $dir): array
 {
     $array = array_diff(scandir($dir), array('.', '..'));
-
     foreach ($array as &$item) {
         $item = $dir . DIRECTORY_SEPARATOR . $item;
     }
     unset($item);
-
     foreach ($array as $item) {
         if (is_dir($item)) {
             $array = array_merge($array, listAllFiles($item));
         }
     }
-
     return $array;
 }
-
 function organizeContent(array $logos, string $source): array
 {
     $output = array();
-
     foreach ($logos as $file) {
         $filename = basename($file);
         $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
-
         if (in_array($ext, ['png'])) {
             $key = preg_replace('/\.png$/i', '', $filename);
             $output['logos'][$key] = $filename;
         }
     }
-
     return $output;
 }
-
 function createMDFiles(array $logos, string $source): void
 {
     global $settings;
-
     foreach ($logos as $files) {
         $outputFile = $source . DIRECTORY_SEPARATOR . $settings['outputFilename'];
-
         echo "Generating $outputFile\n";
-
         $outputContent = "# Logos\n\n";
-
         $table = "";
         $matrix = array();
         $i = 0;
-
         foreach ($files as $fileKey => $file) {
             $matrix[intdiv($i, $settings['cols'])][] = $fileKey;
             $i++;
         }
+        for ($j = 0; $j < count($matrix); $j++) {
+            for ($i = 0; $i < $settings['cols']; $i++) {
+                $logo = $matrix[$j][$i] ?? null;
 
-       for ($i = 0; $i < $settings['cols']; $i++) {
-    $logo = $matrix[$j][$i] ?? null;
+                $table .= '| <div align="center" style="background:#756f6f; padding:10px; border-radius:8px;">';
 
-    $table .= '| <div align="center" style="background:#756f6f; padding:10px; border-radius:8px;">';
+                if ($logo !== null) {
+                    $table .= '<img src="' . $logo . '.png" width="120">';
+                } else {
+                    $table .= '&nbsp;';
+                }
 
-    if ($logo !== null) {
-        $table .= '<img src="' . $logo . '.png" width="120">';
-    }
+                $table .= '</div> ';
 
-    $table .= '</div> ';
-
-    if ($i === $settings['cols'] - 1) {
-        $table .= "|\n";
-    }
-}
-
+                if ($i === $settings['cols'] - 1) {
+                    $table .= "|\n";
+                }
+            }
+            if ($j === 0) {
+                for ($i = 0; $i < $settings['cols']; $i++) {
+                    $table .= "|:---:";
+                    if ($i === $settings['cols'] - 1) {
+                        $table .= "|\n";
+                    }
+                }
+            }
+        }
         $outputContent .= "$table\n";
-
         file_put_contents($outputFile, $outputContent);
     }
 }
-
 function generateAllLogosMosaics(): void
 {
     global $settings;
-
     foreach ($settings['countriesFolders'] as $source) {
         $logos = listAllFiles($source);
         $logos = organizeContent($logos, $source);
         createMDFiles($logos, $source);
     }
 }
-
 generateAllLogosMosaics();
