f09502eaba6147bf3a7ee9bfbe84ce089e4a170b
Rootzpower <15384112+Rootzpower@users.noreply.github.com>
2026-06-11 15:53:08 +0100

    fixs

--- utilities/generate-logos-mosaics-with-names.php
+++ utilities/generate-logos-mosaics-with-names.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * PHP script to generate all logos mosaics.
@@ -14,13 +13,10 @@
  * Tested with PHP 8.4 (cli).
  * ⚠️ Script comes with no warranty, use at your own risk.
  */
-
 error_reporting(E_ALL);
-
 if (PHP_SAPI !== 'cli') {
     die("This script must be ran from the command line.");
 }
-
 $settings = array(
     'countriesFolders' => array(
         __DIR__ . '/../logos',
@@ -28,79 +24,59 @@ $settings = array(
     'outputFilename' => '0_logos_mosaic_with_names.md',
     'cols' => 5,
 );
-
 function listAllFiles(string $dir): array
 {
     $array = array_diff(scandir($dir), array('.', '..'));
-
     foreach ($array as &$item) {
         $item = $dir . DIRECTORY_SEPARATOR . $item;
     }
     unset($item);
-
     foreach ($array as $item) {
         if (is_dir($item)) {
             $array = array_merge($array, listAllFiles($item));
         }
     }
-
     return $array;
 }
-
-function organizeContent(array $logos, string $source): array
+function organizeContent(array $logos): array
 {
     $output = array();
-
     foreach ($logos as $file) {
         $filename = basename($file);
         $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
-
         if ($ext === 'png') {
             $key = preg_replace('/\.png$/i', '', $filename);
             $output['logos'][$key] = $filename;
         }
     }
-
     return $output;
 }
-
 function createMDFiles(array $logos, string $source): void
 {
     global $settings;
-
     foreach ($logos as $files) {
         $outputFile = $source . DIRECTORY_SEPARATOR . $settings['outputFilename'];
-
         echo "Generating $outputFile\n";
-
         $outputContent = "# Logos with names\n\n";
-		$outputContent .= "*To properly view some transparent logos, enable dark mode.*\n\n";
-
+        $outputContent .= "*To properly view some transparent logos, enable dark mode.*\n\n";
         $table = "";
         $matrix = array();
         $i = 0;
-
         foreach ($files as $fileKey => $file) {
             $matrix[intdiv($i, $settings['cols'])][] = $fileKey;
             $i++;
         }
-
         for ($j = 0; $j < count($matrix); $j++) {
-
             // Linha das imagens
             for ($i = 0; $i < $settings['cols']; $i++) {
                 $logo = $matrix[$j][$i] ?? "";
-
-                $table .= '| <div align="center" style="background:#756f6f; padding:10px; border-radius:8px; min-width:140px;">';
-
+                $table .= '| <div align="center">';
                 if ($logo !== "") {
-                    $table .= '<img src="' . $logo . '.png" width="120">';
+                    $table .= '<img src="' . $logo . '.png" width="100" height="60">';
                 }
-
                 $table .= '</div> ';
             }
             $table .= "|\n";
-
             // Header da tabela (só na primeira linha)
             if ($j === 0) {
                 for ($i = 0; $i < $settings['cols']; $i++) {
@@ -108,30 +84,24 @@ function createMDFiles(array $logos, string $source): void
                 }
                 $table .= "|\n";
             }
-
             // Linha dos nomes
             for ($i = 0; $i < $settings['cols']; $i++) {
                 $logo = $matrix[$j][$i] ?? "";
-               $table .= '| <div align="center" style="min-width:140px;"><span style="font-family: monospace; font-size:8px;">' . ($logo !== "" ? $logo : '') . '</span></div> ';
+                $table .= '| <div align="center">' . ($logo !== "" ? $logo : '') . '</div> ';
             }
             $table .= "|\n";
         }
-
         $outputContent .= "$table\n";
-
         file_put_contents($outputFile, $outputContent);
     }
 }
-
 function generateAllLogosMosaics(): void
 {
     global $settings;
-
     foreach ($settings['countriesFolders'] as $source) {
         $logos = listAllFiles($source);
-        $logos = organizeContent($logos, $source);
+        $logos = organizeContent($logos);
         createMDFiles($logos, $source);
     }
 }
-
 generateAllLogosMosaics();
