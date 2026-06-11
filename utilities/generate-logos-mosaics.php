23d4d8183bef6e3872d67bbe03e529c56cfa04c0
Rootz <15384112+Rootzpower@users.noreply.github.com>
2026-06-11 22:32:03 +0100

    Update generate-logos-mosaics.php

--- utilities/generate-logos-mosaics.php
+++ utilities/generate-logos-mosaics.php
@@ -26,7 +26,7 @@ $settings = array(
     'countriesFolders' => array(
         __DIR__ . '/../logos',
     ),
-    'outputFilename' => '0_logos_mosaic.md',
+    'outputFilename' => '0-logos-mosaic.md',
     'cols' => 6,
 );
 
