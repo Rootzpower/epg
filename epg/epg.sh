160a4fddd07c591c8fba28c83ab3fb1bb2ec79fb
Rootzpower <15384112+Rootzpower@users.noreply.github.com>
2026-06-19 18:37:07 +0100

    epg.sh

--- epg/epg.sh
+++ epg/epg.sh
@@ -44,7 +44,7 @@ npm run grab --- --sites=orangetv.orange.es --output=../epg/epg-orangetv-orange-
 npm run grab --- --sites=watch.whaletvplus.com --output=../epg/epg-watch-whaletvplus-com.xml --days=7 --maxConnections=20
 
 # nowplayer.now.com epg
-npm run grab --- --sites=nowplayer.now.com  --lang=en --output=../epg/nowplayer.now.com_en.channels.xml --days=7 --maxConnections=20
+npm run grab --- --sites=nowplayer.now.com  --lang=en --output=../epg/epg-nowplayer-now-en-com.xml --days=7 --maxConnections=20
 
 # ====================================================================================================
 
